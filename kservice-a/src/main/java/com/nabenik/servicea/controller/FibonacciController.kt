ea72458cdfaee0df3ae9a5d6a4f6501321df169b
Víctor Orozco <me@vorozco.com>
2021-08-02 10:38:12 -0600

    Adding linkerd hpa sample

---
+++ kservice-a/src/main/java/com/nabenik/servicea/controller/FibonacciController.kt
@@ -0,0 +1,23 @@
+package com.nabenik.servicea.controller
+
+import javax.enterprise.context.ApplicationScoped
+import javax.ws.rs.*
+
+@Path("/fibonacci")
+@ApplicationScoped
+class FibonacciController{
+
+
+
+    @GET
+    fun doHello(@QueryParam("n") @DefaultValue("0") n: Long): String {
+        return doFibo(n).toString()
+    }
+
+    fun doFibo(n: Long): Long {
+        if(n <= 1) {
+            return n;
+        }
+        return doFibo(n-1) + doFibo(n-2)
+    }
+}
